2d699a419dd1ccb363bc422c4400448ff82c9ddf
Edwin Saens <edwinsaens13@gmail.com>
2024-06-12 19:57:11 -0600

    ejercicios c#

--- #02 FUNCIONES Y ALCANCE/obj/Debug/net8.0/#02 FUNCIONES Y ALCANCE.AssemblyInfo.cs
+++ #02 FUNCIONES Y ALCANCE/obj/Debug/net8.0/#02 FUNCIONES Y ALCANCE.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("#01 OPERADORES Y ESTRUCTURAS DE CONTROL")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("#02 FUNCIONES Y ALCANCE")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ebb0610e10933e3ae6e8f2c7e243617323b31417")]
-[assembly: System.Reflection.AssemblyProductAttribute("#01 OPERADORES Y ESTRUCTURAS DE CONTROL")]
-[assembly: System.Reflection.AssemblyTitleAttribute("#01 OPERADORES Y ESTRUCTURAS DE CONTROL")]
+[assembly: System.Reflection.AssemblyProductAttribute("#02 FUNCIONES Y ALCANCE")]
+[assembly: System.Reflection.AssemblyTitleAttribute("#02 FUNCIONES Y ALCANCE")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.
